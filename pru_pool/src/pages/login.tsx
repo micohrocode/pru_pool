ca66d23961ce4899dfd236d2ac21ea2a423b7ee4
beng2004 <btguerrieri@gmail.com>
2024-06-26 12:55:03 -0400

    Add 2 pages

--- pru_pool/src/pages/login.tsx
+++ pru_pool/src/pages/login.tsx
@@ -1,6 +1,20 @@
+// import { useState } from "react";
 
-const Login = () => {
-    
+const Login : React.FC = () => {
+    // useEffect(() => {
+    //     document.title = "PruPool Login";
+    // }, []);
+
+
+    return (
+        <div className='flex flex-col'>
+            <label className='m-5'>XID</label>
+            <input type="text" className='m-5' />
+            <label className='m-5'>Password</label>
+            <input type="text" className='m-5' />
+        </div>
+    );
+
 }
 
 export default Login;
