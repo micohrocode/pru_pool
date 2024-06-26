b1ecf246dbc845721028d712cace3a13d4439d0c
MicohRo2 <123667453+MicohRo2@users.noreply.github.com>
2024-06-26 12:33:28 -0400

    function base

---
+++ pru_pool/src/backend/function_calls/request_calls.tsx
@@ -0,0 +1,10 @@
+import rides from "../data/requests.json"
+
+function addRequestToRide(request : object){
+    // send request to driver
+    return null;
+}
+
+export {
+    addRequestToRide,
+}
---
+++ pru_pool/src/backend/function_calls/ride_calls.tsx
@@ -0,0 +1,15 @@
+import rides from "../data/rides.json"
+
+function viewAllRides(){
+    // show all available rides
+    return null;
+}
+
+function viewFilteredRides(filters : object){
+    // show all rides that meet the filters
+}
+
+export {
+    viewAllRides,
+    viewFilteredRides
+}
