9c13620b656ab5456d59a78734b10ecb675645ae
beng2004 <btguerrieri@gmail.com>
2024-06-26 16:05:55 -0400

    Finish form

--- pru_pool/src/pages/findride.tsx
+++ pru_pool/src/pages/findride.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CarRoute from '../components/CarRoute';
+import { MapTest } from '../components/MapTest';
 
 const Map: React.FC = () => {
     const routes = [
@@ -10,7 +11,6 @@ const Map: React.FC = () => {
 
     return (
         <div className="flex w-screen">
-            {/* Left panel (30%) */}
             <div className="w-1/3 bg-gray-100 p-4">
                 <h2 className="text-lg text-[#1a1d1e] font-semibold mb-4">Routes</h2>
                 {routes.map((route, index) => (
@@ -23,13 +23,13 @@ const Map: React.FC = () => {
                     />
                 ))}
             </div>
-
-            {/* Right panel (70%) - Map container */}
             <div className="w-2/3 bg-gray-300">
-                {/* Placeholder for the map */}
                 <div className="h-full bg-white p-4">
-                    {/* You can add your map component or integration here */}
-                    Map Container
+                    {/* <MapTest 
+                        props={
+                            
+                        }
+                    /> */}
                 </div>
             </div>
         </div>
